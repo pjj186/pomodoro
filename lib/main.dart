84fb8388adac155512b948f58c7ce819454a899a
Lenny <66498240+pjj186@users.noreply.github.com>
2024-02-26 22:43:06 +0900

    User Interface
    - 뽀모도로 앱 UI 작업

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -22,7 +23,7 @@ class App extends StatelessWidget {
         cardColor: const Color(0xFFF4EDDB),
       ),
       home: const Scaffold(
-        body: Center(),
+        body: HomeScreen(),
       ),
     );
   }
